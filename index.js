2842b7041acb7ce4b00a10b96bd2019ff0833a76
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2021-11-15 19:03:31 +0100

    work

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const discord = require("discord.js")
+const Discord = require("discord.js");
+require("dotenv").config();
+const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+bot.on("ready", async () => {
+  //await train_bot();
+  console.log(
+    `${bot.user.username} is online on ${bot.guilds.cache.size} servers`
+  );
+  bot.user.setActivity("with some code", {
+    type: "PLAYING",
+  });
+});
+
+bot.on('message', msg => { // Message function
+  if (msg.author.bot) return; // Ignore all bots
+  // if (msg.content.startsWith(settings.prefix)) return; // It always has to starts with the prefix which is '!'
+
+  if (msg.content.startsWith("ping")) { // When a player does '!ping'
+    msg.reply("Pong!") // The bot will say @Author, Pong!
+  }
+});
+
+//login
+bot.login(process.env.token);
