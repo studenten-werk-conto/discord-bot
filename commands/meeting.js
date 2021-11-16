8d186ccba4c1adb83a2996f97a8a4bfd555354f3
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2021-11-16 20:32:05 +0100

    working on meeting command

--- commands/meeting.js
+++ commands/meeting.js
@@ -1,4 +1,5 @@
 const errors = require("../utils/errors.js");
+const { GetChannelId } = require("../utils/index.js");
 /**
  * @author cvanh
  * @param {*} bot
@@ -15,17 +16,30 @@ module.exports.run = async (bot, message, args) => {
   //     message.channel.send(botmessage);
   //     }
   const Subcommand = message.content.substring(9);
-  console.log(Subcommand);
+  console.log("the subcommand is: " + Subcommand);
 
   switch (Subcommand) {
-    case "create":
-        const name = "naam"
-        message.guild.createChannel(name, 'text')
+    case "create": // this is for .meeting create 
+      message.guild.channels.fetch().then((channels) => {
+
+        channels.forEach(element => { // loops trough the voice channels 
+
+          // find a empty voice channel
+          if(element.type === "GUILD_VOICE" &&
+          element.members.size === 0
+          ){
+            console.log(element.id);
+            console.log(message.voice)
+            }
+        });
+        
+      });
+      // message.guild.createChannel(name, 'text')
       break;
-    case "remove":
+    case "presentie": // this is for .meeting remove
       break;
-    default:
-      bot.messageCreate("please enter a valid input");
+    default: // no valid command found
+      bot.messageCreate("not a valid input");
   }
 };
 
---
+++ utils/index.js
@@ -0,0 +1,5 @@
+const {GetChannelId} = require("./GetChannelId");
+
+module.exports = {
+    GetChannelId,
+};
