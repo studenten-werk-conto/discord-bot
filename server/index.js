ff168d355ac2f3983703f5e1deabc11117d99386
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2021-11-23 12:44:53 +0100

    work

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const path = require("path");
 const { parse_host } = require("tld-extract");
+const SendConfirmation = require("./utils/SendConfirmation");
 
 const app = express();
 const port = 8080;
 
 // sendFile will go here
-app.get("/", function (req, res) {
+app.get("/index.html", function (req, res) {
   res.sendFile(path.join(__dirname, "/track.html"));
 });
 
 app.get("/track", async (req, res) => {
   console.log(req.query);
   console.log(req.query.email);
-
+  SendConfirmation(req.query.email, uuid);
   console.log(parse_host(req.query.email));
 
   res.status(200).send({ response: req.body });
